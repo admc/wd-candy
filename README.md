4f88158af682a3a156ae37af015ff80654d4eda2
Adam Christian <adam.christian@gmail.com>
2012-04-17 14:23:45 +0100

    Getting the codes out

---
+++ README.md
@@ -0,0 +1,16 @@
+# wd-candy, a wrapper adding awesome syntax to wd the webdriver client for node
+
+## Update node to latest
+
+http://nodejs.org/#download
+
+## Install
+
+<pre>
+npm install wd
+</pre>
+
+## Authors
+
+  - Adam Christian ([admc](http://github.com/admc))
+
---
+++ tests/test.basic.js
@@ -0,0 +1,26 @@
+var wd = require("./lib/main")
+var go = require('chain-tiny');
+
+var b = wd.candy();
+
+go(function(next) {
+  b.add('init', {browserName:'chrome'}, next);
+})
+.go(function(res, next) {
+  b.add('get', 'http://www.admc.io', next);
+})
+.go(function(res, next) {
+  b.add('element', ['link text', 'GitHub'], next);
+})
+.go(function(res, next) {
+  b.add('moveTo', [res, 0, 0], next);
+})
+.go(function(res, next) {
+  b.add('click', 0, next);
+})
+.go(function(res, next) {
+  b.add('eval', "window.location.href", next);
+})
+.end(function(err, res) {
+  b.add('quit', null);
+});
